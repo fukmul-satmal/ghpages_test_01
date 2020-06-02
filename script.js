f256285ebc10de65945df88bacf158225a0a5df5
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-06-02 10:38:16 +0900

    edit script.js

--- script.js
+++ script.js
@@ -129,8 +129,8 @@ const base64URLEncode = (str) => {
 }
 
 
-const sha256 = async (buffer) => {
-    let hashbuffer = await window.crypto.subtle.digest('SHA-256', new TextEncoder().encode(buffer));
+const sha256 = (buffer) => {
+    let hashbuffer = window.crypto.subtle.digest('SHA-256', new TextEncoder().encode(buffer));
     let hasharray = Array.from(new Uint8Array(hashbuffer));
     let hashHex = hasharray.map(b => b.toString(16).padStart(2, '0')).join('');
     console.log("hashHex is " + HashHex);
