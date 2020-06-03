ab9e3a30f1fdc13419f8c555d6287a0c0705ee01
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-06-03 16:27:58 +0900

    edit script.js and index.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var asn1 = exports;
 
 asn1.bignum = require('bn.js');
@@ -29175,7 +29175,7 @@ exports.createContext = Script.createContext = function (context) {
     return copy;
 };
 
-},{}],193:[function(require,module,exports){
+},{}],"loginmodule":[function(require,module,exports){
 'use strict';
 
 const crypto = require("crypto");
@@ -29517,4 +29517,4 @@ const updateUI = () => {
 };
 
 
-},{"crypto":73}]},{},[193]);
+},{"crypto":73}]},{},[]);
